621c6f3e78ffab595abf2a17b7715f90d577ae4f
martin-centurion <martinalejandrocenturion@gmail.com>
2023-10-06 01:09:49 -0300

    clase8

--- 08-router-y-multer/router-demo/src/app.js
+++ 08-router-y-multer/router-demo/src/app.js
@@ -1,14 +1,14 @@
 import express from 'express';
 
+import userRouter from './routers/users.router.js';
+
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello People!</h2>')
-});
+app.use('/api', userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is runnimg on http://localhost:${PORT}`);
