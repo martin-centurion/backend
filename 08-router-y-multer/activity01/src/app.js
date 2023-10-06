b307dd33ba80eaba50462d9894d3fe3e6bf737bb
martin-centurion <martinalejandrocenturion@gmail.com>
2023-10-06 01:35:19 -0300

    clase8 - activity-01

---
+++ 08-router-y-multer/activity01/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import userRouter from './routers/users.router.js';
+import petsRouter from './routers/pets.router.js'
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', userRouter, petsRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is runnimg on http://localhost:${PORT}`);
+});
---
+++ 08-router-y-multer/activity01/src/routers/pets.router.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { v4 as uuidv4 } from "uuid";
+
+const router = Router();
+
+const pets = [
+    {
+        id: 'e9edb299-ceab-43d2-bc04-9be1d69e901d',
+        name: 'Snowball',
+        race: 'Maltez',
+        age: '3',
+        gender: 'M'
+    },
+];
+
+router.get('/pets', (req, res) => {
+    res.status(200).json(pets);
+});
+
+router.post('/pets', (req, res) => {
+    const { body } = req;
+    const newPet = {
+        id: uuidv4(),
+        ... body,
+    }
+    pets.push(newPet);
+    res.status(201).json(newPet);
+})
+
+export default router;
