b340e58b19b5fdaf2b1d721a039017ab353b7e70
martin-centurion <martinalejandrocenturion@gmail.com>
2023-10-09 21:57:49 -0300

    clase 8 practica

---
+++ 08-router-y-multer/multer-demo/src/routers/pets.router.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import path from 'path';
+import { v4 as uuidv4 } from "uuid";
+import { uploader } from '../utils.js';
+
+const router = Router();
+
+const pets = [
+    {
+        id: 'e9edb299-ceab-43d2-bc04-9be1d69e901d',
+        name: 'Snowball',
+        race: 'Maltez',
+        age: '3',
+        gender: 'M'
+    },
+];
+
+router.get('/pets', (req, res) => {
+    res.status(200).json(pets);
+});
+
+router.post('/pets', uploader.single('thumbnail'), (req, res) => {
+    const { body } = req;
+    const newPet = {
+        id: uuidv4(),
+        ... body,
+        thumbnail: req.file.path,
+    };
+    pets.push(newPet);
+    res.status(201).json(newPet);
+});
+
+export default router;
---
+++ 08-router-y-multer/multer-demo/src/utils.js
@@ -0,0 +1,19 @@
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const folderPath = path.join(__dirname, '../public/img')
+        cb(null, folderPath);
+    },
+    filename: (req, file, cb) => {
+        const filename = `${Date.now()} - ${file.originalname}`;
+        cb(null, filename);
+    }
+});
+
+export const uploader = multer({ storage })
