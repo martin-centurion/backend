0c5c0b93d9e19449fd086936d739c23e8e8b7e47
martin-centurion <martinalejandrocenturion@gmail.com>
2023-11-12 16:32:06 -0300

    actualizacion server-base

--- 09-motores-de-plantillas/server-base/src/app.js
+++ 09-motores-de-plantillas/server-base/src/app.js
@@ -12,7 +12,7 @@ const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/static' ,express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 
