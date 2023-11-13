6e049cd51a798539a2b005d4c8d9d7f01d30da55
martin-centurion <martinalejandrocenturion@gmail.com>
2023-11-13 20:36:34 -0300

    create repo mongo db

---
+++ 14-mongoose/mongodb-demo/src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path'; 
+import { fileURLToPath } from 'url';
+
+import indexRouter from './routers/index.router.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', indexRouter);
+
+app.use((error, req, res, next) => {
+    const message = `Ah ocurrido un error desconocido: ${error.message}`;
+    console.log(message);
+    res.status(500).json({ status: 'error', message });
+});
+
+export default app;
