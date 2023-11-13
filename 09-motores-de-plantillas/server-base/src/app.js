0832edf7a1cca1f26d8830ad477ecf46263c4257
martin-centurion <martinalejandrocenturion@gmail.com>
2023-11-13 20:55:52 -0300

    server-base correccion

--- 09-motores-de-plantillas/server-base/src/app.js
+++ 09-motores-de-plantillas/server-base/src/app.js
@@ -1,14 +1,10 @@
 import express from 'express';
 import path from 'path'; 
-import { fileURLToPath } from 'url';
+import { __dirname } from './utils.js';
 
 import indexRouter from './routers/index.router.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
-const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
