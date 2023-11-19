b010339ada87a14bd19c660c8bf465bd0ff187d6
martin-centurion <martinalejandrocenturion@gmail.com>
2023-11-19 13:12:43 -0300

    config api/products

--- segunda-pre-entrega/src/app.js
+++ segunda-pre-entrega/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import productViewRouter from './routers/views/products.router.js';
+import productApiRouter from './routers/api/productsApi.router.js';
 import { __dirname } from './utils.js';
 import path from 'path';
 
@@ -21,8 +22,8 @@ app.get('/', (req, res) => {
 app.use('/home', (req, res) => {
     res.render('index');
 });
-
-app.use('/products', productViewRouter);
+app.use('/api', productApiRouter)
+app.use('/', productViewRouter);
 
 
 export default app;
