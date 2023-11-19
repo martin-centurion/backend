05f9ff1105c0b6d5d10c293c2f8172d27bb01a69
martin-centurion <martinalejandrocenturion@gmail.com>
2023-11-19 12:54:58 -0300

    create model and productmanager

---
+++ segunda-pre-entrega/src/models/product.model.js
@@ -0,0 +1,13 @@
+import mongoose, { mongo } from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    thumbnails: { type: String},
+    code: { type: String, required: true },
+    category: { type: String, required: true },
+    status: { type: Boolean, required: true, default: true },
+}, { timestamps: true });
+
+export default mongoose.model('Product', productSchema);
