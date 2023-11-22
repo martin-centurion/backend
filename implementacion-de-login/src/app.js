bbe9d82596f6d18ebf85507b0cfbcec766a730b7
martin-centurion <martinalejandrocenturion@gmail.com>
2023-11-22 20:57:43 -0300

    test cookie

--- implementacion-de-login/src/app.js
+++ implementacion-de-login/src/app.js
@@ -5,18 +5,19 @@ import productApiRouter from './routers/api/productsApi.router.js';
 import cookieParser from 'cookie-parser';
 import cartViewRouter from './routers/views/carts.router.js';
 import cartApiRouter from './routers/api/cartsApi.router.js';
-
-import indexRouter from './routers/index.router.js';
+import cookieRouter from './routers/views/cookie.router.js';
 
 import { __dirname } from './utils.js';
 import path from 'path';
+
+const COOKIE_SECRET = '5(HWwTw%£_Q%<&RMEEiK6r7tLg]-$l@o';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(cookieParser());
+app.use(cookieParser(COOKIE_SECRET));
 
 app.engine('handlebars', handlebars.engine());
 app.set('views', path.join(__dirname, 'views'));
@@ -26,7 +27,7 @@ app.use('/', cartApiRouter);
 app.use('/', productApiRouter)
 app.use('/', productViewRouter, cartViewRouter);
 
-app.use('/', indexRouter);
+app.use('/', cookieRouter);
 
 
 export default app;
