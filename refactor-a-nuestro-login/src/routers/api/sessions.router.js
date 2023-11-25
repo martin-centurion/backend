d8e25977b08b2eb28fe0f62441851fdc4d1e339a
martin-centurion <martinalejandrocenturion@gmail.com>
2023-11-25 00:37:25 -0300

    clear repo refactor

--- refactor-a-nuestro-login/src/routers/api/sessions.router.js
+++ refactor-a-nuestro-login/src/routers/api/sessions.router.js
@@ -5,16 +5,6 @@ import UserModel from '../../models/user.model.js'
 
 const router = Router();
 
-/* router.post('/sessions/register', async (req, res) => {
-    const { body } = req;
-    const newUser = await UserModel.create({ 
-        ...body, 
-        password: createHash(body.password)
-    });
-    console.log('newUser', newUser);
-    res.redirect('/login');
-}); */
-
 router.post('/sessions/register', passport.authenticate('register', { failureRedirect: '/register' }), (req, res) => {
     res.redirect('/login');
 });
@@ -33,21 +23,6 @@ router.post('/sessions/recovery-password', async (req, res) => {
     await UserModel.updateOne({ email }, { $set: { password: createHash(newPassword) }});
     res.redirect('/login');
 });
-
-/* router.post('/sessions/login', async (req, res) => {
-    const { body: { email, password } } = req;
-    const user = await UserModel.findOne({ email });
-    if (!user) {
-        return res.status(401).send('Correo o contraseña invalidos.')
-    };
-    const isPassValid = isValidPassword(password, user);
-    if (!isPassValid) {
-        return res.status(401).send('Correo o contraseña invalidos.')
-    };
-    const { first_name, last_name } = user;
-    req.session.user = { first_name, last_name, email };
-    res.redirect('/products');
-}); */
 
 router.get('/sessions/logout', (req, res) => {
     req.session.destroy((error) => {
