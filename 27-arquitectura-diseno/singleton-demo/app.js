75883ffebb74eec35dbad2c0cd2d96714fc65705
martin-centurion <martinalejandrocenturion@gmail.com>
2023-12-10 17:33:32 -0300

    repaso clase 27

---
+++ 27-arquitectura-diseno/singleton-demo/app.js
@@ -0,0 +1,6 @@
+import MongoSingleton from "./MongoSingleton.js";
+
+const mongoSingletonInstance1 = MongoSingleton.getIntance();
+const mongoSingletonInstance2 = MongoSingleton.getIntance();
+const mongoSingletonInstance3 = MongoSingleton.getIntance();
+const mongoSingletonInstance4 = MongoSingleton.getIntance();
