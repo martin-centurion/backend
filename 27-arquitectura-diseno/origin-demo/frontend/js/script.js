277f6da1c0ceb3ee575555b97fb919bec0063fe1
martin-centurion <martinalejandrocenturion@gmail.com>
2024-01-29 17:37:58 -0300

    repaso clase 27

--- 27-arquitectura-diseno/origin-demo/frontend/js/script.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    fetch('http://localhost:8080/origin-demo')
-        .then(response => response.json())
-        .then(data => console.log('data', data))
-        .catch(console.error)
-})();
--- 27-arquitectura-diseno/singleton-demo/app.js
+++
@@ -1,6 +0,0 @@
-import MongoSingleton from "./MongoSingleton.js";
-
-const mongoSingletonInstance1 = MongoSingleton.getIntance();
-const mongoSingletonInstance2 = MongoSingleton.getIntance();
-const mongoSingletonInstance3 = MongoSingleton.getIntance();
-const mongoSingletonInstance4 = MongoSingleton.getIntance();
