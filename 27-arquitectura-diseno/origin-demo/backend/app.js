5dfdc527769a7c6f7b26ae26abf1ed79b701d39f
martin-centurion <martinalejandrocenturion@gmail.com>
2024-01-29 20:13:05 -0300

    cors

--- 27-arquitectura-diseno/origin-demo/backend/app.js
+++ 27-arquitectura-diseno/origin-demo/backend/app.js
@@ -5,7 +5,23 @@ import cors from 'cors';
 const app = express();
 const PORT = 8080;
 
-app.use(cors());
+const whiteList = [
+    'http://localhost:5500', // Admin
+    'http://localhost:8081', // Store
+    'http://localhost:3001' // Auditory
+]
+
+//app.use(cors()); // Se habilita a todo el sitio
+app.use(cors({
+    origin: function(origin, callback) {
+        console.log('origin', origin);
+        if (whiteList.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+})); // Solo se habilita al dominio indicado
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
