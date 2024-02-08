c0de8ef81f572283198b61b85f9c5289cb6ec264
martin-centurion <martinalejandrocenturion@gmail.com>
2024-02-08 17:52:01 -0300

    reapaso clase 34

---
+++ 34-logging-y-performance/winston-demo/src/server.js
@@ -0,0 +1,13 @@
+import http from 'http';
+import { init as initMongoDB } from './db/mongodb.js';
+import app from './app.js';
+import config from './config/config.js';
+
+await initMongoDB();
+
+const server = http.createServer(app);
+const PORT = config.PORT;
+
+server.listen(PORT, () => {
+    console.log(`Server running in http://localhost:${PORT}`);
+});
