3700caf152462665ef4ed57b6679af0e49a06b84
martin-centurion <martinalejandrocenturion@gmail.com>
2024-02-08 19:58:34 -0300

    config loggers

--- 34-logging-y-performance/activity-01/src/config/logger.js
+++ 34-logging-y-performance/activity-01/src/config/logger.js
@@ -1,10 +1,34 @@
 import winston from 'winston';
 import config from './config.js';
 
+const customLevelsOptions = {
+    levels: {
+        fatal: 0,
+        error: 1,
+        warning: 2,
+        info: 3,
+        debug: 4,
+    },
+    colors: {
+        fatal: 'red',
+        error: 'magenta',
+        warning: 'yellow',
+        info: 'blue',
+        debug: 'white',
+    }
+}
+
 const loggerProd = winston.createLogger({
+    levels: customLevelsOptions.levels,
     transports: [
-        new winston.transports.Console({ level: 'http' }),
-        new winston.transports.File({ filename: './error.log', level: 'warn' }),
+        new winston.transports.Console({ 
+            level: 'debug',
+            format: winston.format.combine(
+                winston.format.colorize({ colors: customLevelsOptions.colors }),
+                winston.format.simple()
+            ), 
+        }),
+        //new winston.transports.File({ filename: './error.log', level: 'warn' }),
     ],
 });
 
--- 34-logging-y-performance/activity-01/src/routers/views/index.router.js
+++ 34-logging-y-performance/activity-01/src/routers/views/index.router.js
@@ -7,14 +7,16 @@ router.get('/', (req, res) => {
 });
 
 router.get('/loggers', (req, res) => {
-  req.logger.error('Esta es una prueba de log error.');
-  req.logger.warn('Este es una prueba de log warn.');
+  req.logger.fatal('Esta es una prueba de log fatal.');
+  req.logger.error('Este es una prueba de log error.');
+  req.logger.warning('Este es una prueba de log warning.');
   req.logger.info('Este es una prueba de log info.');
-  req.logger.http('Este es una prueba de log http.');
+  /* req.logger.http('Este es una prueba de log http.');
   req.logger.verbose('Este es una prueba de log verbose.');
   req.logger.debug('Este es una prueba de log debug.')
-  req.logger.silly('Este es una prueba de log silly.')
+  req.logger.silly('Este es una prueba de log silly.') */
   res.status(200).json('Ok!');
 });
 
 export default router;
+
