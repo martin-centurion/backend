6368dbc9bb1f34b536242121b40523e6519882c4
martin-centurion <martinalejandrocenturion@gmail.com>
2024-02-16 21:33:15 -0300

    repaso clase 35

--- 34-logging-y-performance/winston-demo/src/routers/views/index.router.js
+++ 34-logging-y-performance/winston-demo/src/routers/views/index.router.js
@@ -6,4 +6,10 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'Hello People 🖐️' });
 });
 
+router.get('/loggers', (req, res) => {
+  req.logger.error('Esta es una prueba de log error.');
+  req.logger.warn('Esta es una prueba de log warn.');
+  res.status(200).send('Ok');
+});
+
 export default router;
