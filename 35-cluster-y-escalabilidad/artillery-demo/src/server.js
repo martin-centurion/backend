182a7cbb6018adac3dfe4e1ec6e573a2a864c528
martin-centurion <martinalejandrocenturion@gmail.com>
2024-02-16 21:41:46 -0300

    repaso clase 35

--- 35-cluster-y-escalabilidad/artillery-demo/src/server.js
+++ 35-cluster-y-escalabilidad/artillery-demo/src/server.js
@@ -1,14 +1,28 @@
 import http from 'http';
 import cluster from 'cluster';
-
-console.log(`PID ${process.pid} : cluster.isPrimary: ${cluster.isPrimary}`);
+import { cpus } from 'os';
 
 import app from './app.js';
-import { log } from 'console';
 
-const server = http.createServer(app);
-const PORT = 8080;
-
-server.listen(PORT, () => {
-    console.log(`Server running in http://localhost:${PORT}`);
-});
+console.log(`PID ${process.pid}: cluster.isPrimary -> ${cluster.isPrimary}`);
+if (cluster.isPrimary) {
+  const cpusNumber = cpus().length;
+  console.log(`Soy el proceso principal con PID ${process.pid}. Y dispongo de ${cpusNumber} procesadores.`);
+  for (let i = 0; i < cpusNumber; i++){
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`El worker ${worker.process.pid} ha muerto`, code, signal);
+    if (String(signal) !== 'SIGTERM') {
+      cluster.fork();
+    }
+  });
+} else {
+  console.log(`Soy un proceso worker con PID ${process.pid}`);
+  const server = http.createServer(app);
+  const PORT = 8080;
+  
+  server.listen(PORT, () => {
+    console.log(`Server running in http://localhost:${PORT} 🚀`);
+  });
+}
