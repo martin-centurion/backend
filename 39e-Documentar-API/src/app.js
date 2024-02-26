8eab8c3ae6a419becf055b2084860b1670bc4fd2
martin-centurion <martinalejandrocenturion@gmail.com>
2024-02-25 23:33:49 -0300

    entrega swagger

--- 39e-Documentar-API/src/app.js
+++ 39e-Documentar-API/src/app.js
@@ -4,6 +4,8 @@ import handlebars from 'express-handlebars';
 import { init as initPassportConfig } from './config/passport.config.js';
 import expressCompression from 'express-compression';
 import { addLogger } from './config/logger.js';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 // Api
 import cookieParser from 'cookie-parser';
@@ -43,6 +45,20 @@ app.set('view engine', 'handlebars');
 
 initPassportConfig();
 app.use(passport.initialize());
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.1',
+        info: {
+            title: 'Swatch Api',
+            description: 'Esta es la documentación de la API de Swatch (Curso Coderhouse - Backend)',
+        },
+    },
+    apis: [ path.join(__dirname, 'docs', '**', '*.yaml') ]
+};
+
+const specs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 
 app.use('/',
     indexViewsRouter,
